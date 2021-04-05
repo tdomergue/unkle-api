ebb22e6c541f1433504fd97c36499f06230b7bd4
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-04-05 16:11:56 +0200

    tried to create Subscriptions and ContractOptions in the same request than a Contract..failed

--- app/controllers/api/v1/contracts_controller.rb
+++ app/controllers/api/v1/contracts_controller.rb
@@ -1,15 +1,19 @@
+require 'pry-byebug'
+
 class Api::V1::ContractsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
   def create
+    binding.pry
     @contract = Contract.new(contract_params)
-    @subscription = Subscription.new(subscription_params)
-    p subscription_params
-    @subscription.contract = @contract
     authorize @contract
-    authorize @subscription
-    if @contract.save && @subscription.save
-      render json: { success: "contract and subscription created!" }, status: :created
+    if @contract.save
+      subscription_params['user_id'].each do |subscriber|
+        @subscription = Subscription.new(subscriber, @contract)
+        authorize @subscription
+        render_subscription_error unless @subscription.save
+      end
+      render json: { success: "contract created! Now attach client(s) and option(s) by creating Subscription(s) and ContractOption(s)..." }, status: :created
       # render :show, status: :created
     else
       render_error
@@ -18,16 +22,21 @@ class Api::V1::ContractsController < Api::V1::BaseController
 
   private
 
+  def contract_params
+    params.require(:contract).permit(:number, :start_date, :end_date)
+  end
+  
+  def subscription_params
+    params.require(:subscription).permit(:user_id)
+  end
+
   def render_error
     render json: { errors: @contract.errors.full_messages },
       status: :unprocessable_entity
   end
 
-  def contract_params
-    params.require(:contract).permit(:number, :start_date, :end_date)
-  end
-
-  def subscription_params
-    params.require(:subscription).permit(:user_id, :contract_id)
+  def render_subscription_error
+    render json: { errors: @subscription.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/views/api/v1/contracts/inputs_helpers/create_contract.rb
+++ app/views/api/v1/contracts/inputs_helpers/create_contract.rb
@@ -4,8 +4,9 @@ require 'json'
 response = RestClient.post( 
   'http://localhost:3000/api/v1/contracts', 
   JSON.generate({
-    "contract": { "number": "3628738298", "start_date": "19/06/1991", "end_date": "19/06/2031" },
-    "subscription": { "user_id": "2" }
+    "contract": { "number": "37289283", "start_date": "19/06/1991", "end_date": "19/06/2031" },
+    "subscription": { "user_id": "1"},
+    "subscription": { "user_id": "2"}
   }),
   :content_type => :json, :accept => :json, :'x-user-email' => "thibaud.domergue@hei.fr", :'x-user-token' => 'Po57BBj2mGzfT8YQB4gy')
 
