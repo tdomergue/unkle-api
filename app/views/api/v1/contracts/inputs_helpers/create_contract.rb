b3f4cb3e0b364d87287d45082354b527bc963243
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-04-06 09:17:56 +0200

    master still working even with the full creation branch

--- app/views/api/v1/contracts/inputs_helpers/create_contract.rb
+++ app/views/api/v1/contracts/inputs_helpers/create_contract.rb
@@ -3,7 +3,7 @@ require 'json'
 
 response = RestClient.post( 
   'http://localhost:3000/api/v1/contracts', 
-  JSON.generate({ "contract": { "number": "7391838938", "start_date": "19/06/1991", "end_date": "19/06/2031" } }),
+  JSON.generate({ "contract": { "number": "79382093", "start_date": "19/06/1991", "end_date": "19/06/2031" } }),
   :content_type => :json, :accept => :json, :'x-user-email' => "thibaud.domergue@hei.fr", :'x-user-token' => 'Po57BBj2mGzfT8YQB4gy')
 
 puts response
